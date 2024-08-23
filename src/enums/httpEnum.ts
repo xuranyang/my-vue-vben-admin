151e06f2ee42d55e254b94fd7dbbd8b2f8b03cfc
xry <362438997@qq.com>
2024-08-24 06:18:45 +0800

    ResultEnum.SUCCESS=0 -> 200

--- src/enums/httpEnum.ts
+++ src/enums/httpEnum.ts
@@ -2,7 +2,9 @@
  * @description: Request result set
  */
 export enum ResultEnum {
-  SUCCESS = 0,
+  // SUCCESS = 0,
+  // TODO SUCCESS 改为 200
+  SUCCESS = 200,
   ERROR = -1,
   TIMEOUT = 401,
   TYPE = 'success',
